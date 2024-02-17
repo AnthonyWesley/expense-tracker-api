05dda7b06e2958ef2f773b908a201e85be0e7e19
Anthony Wesley <awl-7@live.com>
2024-02-17 00:31:15 -0300

    expense-tracker 1.0.4

--- src/instances/pg.ts
+++ src/instances/pg.ts
@@ -3,24 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-let { SB_PASSWORD, SB_HOST, SB_USER, SB_PORT, SB_DB } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 export const sequelize = new Sequelize(
-  `postgres://postgres.ppojpgqwcqigxlgcoigl:${SB_PASSWORD}@aws-0-sa-east-1.pooler.supabase.com:5432/postgres`,
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`,
   {
-    host: "aws-0-sa-east-1.pooler.supabase.com",
+    host: "0.0.0.0",
     dialect: "postgres",
     port: parseInt(process.env.PG_PORT as string),
   }
 );
-
-// let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
-
-// export const sequelize = new Sequelize(
-//   `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`,
-//   {
-//     host: "0.0.0.0",
-//     dialect: "postgres",
-//     port: parseInt(process.env.PG_PORT as string),
-//   }
-// );
