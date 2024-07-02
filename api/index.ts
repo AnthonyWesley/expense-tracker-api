57679d84349ef00c11850a3fd8727d4115cffb25
AnthonyWesley <awl-live.com>
2024-07-02 10:26:44 -0300

    Expense Tracker Api 2.0.1

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
-
 import cors from "cors";
+import dotenv from "dotenv";
 import router from "../src/route/expensesRoutes";
+
+import express from "express";
+dotenv.config();
 
 const server = express();
 
